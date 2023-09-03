69fe8632fa415cea83a4a1d00c8e59651d29d233
zergolf1994 <zergolf1994@gmail.com>
2023-09-03 14:51:45 +0700

    fix create

--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ const { streamTS, streamMP4 } = require("./controllers/stream");
 router.route("/:videoId/:item.(txt|html|png|jpg)").get(streamTS);
 router.route("/:videoId.mp4").get(streamMP4);
 
+const { serverCreate } = require("./controllers/server");
+router.get("/server/create", serverCreate);
+
 router.all("*", async (req, res) => {
   return res.status(404).end();
 });
