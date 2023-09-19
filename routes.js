8181e8c64335827546d59aaf9d837a392930bd3a
zergolf1994 <zergolf1994@gmail.com>
2023-09-19 23:44:53 +0700

    fix loadbalance

--- routes.js
+++ routes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const { streamTS, streamMP4 } = require("./controllers/stream");
 
 router.route("/:videoId/:item.(txt|html|png|jpg)").get(streamTS);
-router.route("/:videoId.mp4").get(streamMP4);
+//router.route("/:videoId.mp4").get(streamMP4);
 
 const { serverCreate } = require("./controllers/server");
 router.get("/server/create", serverCreate);
 
 router.all("*", async (req, res) => {
-  return res.status(404).end();
+  return res.status(200).end('streaming by zembed.xyz');
 });
 
 module.exports = router;
